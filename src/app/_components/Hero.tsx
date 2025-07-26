dfda862d702d732a653f5ff70d283f4aa0a08335
husam-mumin <husammumin@gmail.com>
2025-07-26 20:48:58 +0200

    fix the Hero

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -20,7 +20,7 @@ export default function Hero() {
           particleColor="#FFFFFF"
         />
       </div>
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+      <div className="flex justify-center items-center flex-col  p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50"></h1>
         <Image
           className="filter invert mx-auto w-fit"
